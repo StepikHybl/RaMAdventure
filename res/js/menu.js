0ba876238e08605df4a6441c46b554984ee9208a
Stepik_Hybl <stepan.hyb1@emai.cz>
2022-12-21 22:10:56 +0100

    first commit

---
+++ res/js/menu.js
@@ -0,0 +1,135 @@
+const main = document.getElementById("main");
+const game1 = document.getElementById("game1");
+const game2 = document.getElementById("game2");
+const shooter = document.getElementById("Shooter");
+const back = document.getElementById("back");
+
+const canvas = document.getElementById("canvas1");
+const ctx = canvas.getContext("2d");
+canvas.height = window.innerHeight;
+canvas.width = window.innerWidth;
+
+const collisionCanvas = document.getElementById("collisionCanvas");
+const collisionCtx = collisionCanvas.getContext("2d");
+collisionCanvas.height = window.innerHeight;
+collisionCanvas.width = window.innerWidth;
+
+let timeToNextRaven = 0;
+let ravenInterval = 500;
+let lastTime = 0;
+let score = 0;
+ctx.font = "50px Impact";
+
+let ravens = [];
+class Raven {
+  constructor() {
+    this.spriteWidth = 250;
+    this.spriteHeight = 314;
+    this.sizeModifier = Math.random() * 0.6 + 0.4;
+    this.width = this.spriteWidth * this.sizeModifier;
+    this.height = this.spriteHeight * this.sizeModifier;
+    this.x = canvas.width;
+    this.y = Math.random() * canvas.height - this.height;
+    this.directionX = Math.random() * 5 + 3;
+    this.directionY = Math.random() * 5 - 2.5;
+    this.markedForDeletion = false;
+    this.image = new Image();
+    this.image.src = "./res/img/strileni1.png";
+    this.frame = 0;
+    this.maxFrame = 2;
+    this.timeSinceFlap = 0;
+    this.flapInterval = Math.random() * 10 + 10;
+    this.randomColors = [
+      Math.floor(Math.random() * 255),
+      Math.floor(Math.random() * 255),
+      Math.floor(Math.random() * 255),];
+      this.color = "rgb(" + this.randomColors[0] + ',' + this.randomColors[1] + "," + this.randomColors[2] + ")";
+  }
+
+  uptade(deltatime) {
+    if (this.y < 0 || this.y > canvas.height - this.height) {
+      this.directionY = this.directionY * -1;
+    }
+    this.x -= this.directionX;
+    this.y += this.directionY;
+    this.x -= this.directionX;
+    if (this.x < 0 - this.width) this.markedForDeletion = true;
+    this.timeSinceFlap += deltatime;
+    if (this.timeSinceFlap > this.flapInterval) {
+      if (this.frame > this.maxFrame) this.frame = 0;
+      else this.frame++;
+      this.timeSinceFlap = 0;
+    }
+  }
+
+  draw() {
+    collisionCtx.fillStyle = this.color
+    collisionCtx.fillRect(this.x, this.y, this.width, this.height);
+    ctx.drawImage(
+      this.image,
+      this.frame * this.spriteWidth,
+      0,
+      this.spriteWidth,
+      this.spriteHeight,
+      this.x,
+      this.y,
+      this.width,
+      this.height
+    );
+  }
+}
+
+function drawScore() {
+  ctx.fillStyle = "Black";
+  ctx.fillText("Score: " + score, 50, 75);
+  ctx.fillStyle = "White";
+  ctx.fillText("Score: " + score, 55, 80);
+}
+
+window.addEventListener("click", function (e) {
+  const detectPixelColor = collisionCtx.getImageData(e.x, e.y, 1, 1);
+  console.log(detectPixelColor);
+  const pc = detectPixelColor.data;
+  ravens.forEach(object => {
+    if(object.randomColors[0] === pc[0] && object.randomColors[1] === pc[1]
+        && object.randomColors[2] === pc[2]){
+            object.markedForDeletion = true;
+            score++;
+        }
+  });
+});
+
+function animate(timestamp) {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  collisionCtx.clearRect(0, 0, canvas.width, canvas.height);
+  let deltatime = timestamp - lastTime;
+  lastTime = timestamp;
+  timeToNextRaven += deltatime;
+  if (timeToNextRaven > ravenInterval) {
+    ravens.push(new Raven());
+    timeToNextRaven = 0;
+    ravens.sort(function(a,b){
+        return a.width - b.width;
+    });
+  }
+  drawScore();
+  [...ravens].forEach((object) => object.uptade());
+  [...ravens].forEach((object) => object.draw());
+  ravens = ravens.filter((object) => !object.markedForDeletion);
+
+  requestAnimationFrame(animate);
+}
+
+animate(0);
+
+game1.onclick = () => {
+  document.body.style.backgroundColor = "Green"
+  main.style.display = "none";
+  shooter.style.display = "block";
+};
+
+back.onclick = () => {
+  document.body.style.backgroundColor = "white";
+  main.style.display = "block";
+  shooter.style.display = "none";
+};
